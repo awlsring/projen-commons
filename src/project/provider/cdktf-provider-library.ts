c1a92f0b6ca7c3fe458681c926dff51c39dd4ef2
awlsring <mattcanemail@gmail.com>
2023-03-04 23:40:58 -0800

    feat: provider library project

---
+++ src/project/provider/cdktf-provider-library.ts
@@ -0,0 +1,114 @@
+import { existsSync, readFileSync } from 'fs';
+import * as path from 'path';
+import { JsonFile } from 'projen';
+import { CheckForUpgradesScriptFile } from './check-for-upgrade';
+import { ProviderUpgrade } from './provider-upgrade';
+import { AwlsringCdktfLibrary, AwlsringCdktfLibraryOptions } from '../cdktf-library';
+
+interface CdktfProvider {
+  readonly owner: string;
+  readonly name: string;
+  readonly version: string;
+}
+
+interface CdktfProviderLibraryOptions extends AwlsringCdktfLibraryOptions {
+  readonly provider: CdktfProvider;
+}
+
+export class AwlsringCdktfProviderLibrary extends AwlsringCdktfLibrary {
+  readonly cdktfJson = 'cdktf.json';
+
+  constructor(props: CdktfProviderLibraryOptions) {
+    super(props);
+
+    const providerName = `${props.provider.owner}/${props.provider.name}`;
+
+    this.addPeerDeps(`cdktf@${props.cdktfVersion}`);
+    this.addPeerDeps(`constructs@${props.constructsVersion}`);
+
+    this.addDevDeps(`cdktf@${props.cdktfVersion}`);
+    this.addDevDeps(`cdktf-cli@${props.cdktfVersion}`);
+    this.addDevDeps(`constructs@${props.constructsVersion}`);
+
+    const versionFile = path.resolve(this.outdir, 'src/version.json');
+
+    let fullyQualifiedProviderName = '<unknown>';
+    let actualProviderVersion = '<unknown>';
+    if (existsSync(versionFile)) {
+      const map = JSON.parse(readFileSync(versionFile, 'utf8'));
+      if (Object.keys(map).length !== 1) {
+        throw new Error(
+          'version.json must have exactly one entry. specifying multiple providers is not supported',
+        );
+      }
+      const [fqpn, version] = Object.entries(map)[0];
+      fullyQualifiedProviderName = fqpn;
+      actualProviderVersion = version as string;
+    }
+
+    this.addFields({
+      cdktf: {
+        provider: {
+          name: fullyQualifiedProviderName,
+          version: actualProviderVersion,
+        },
+      },
+    });
+
+    const upgradeScript = new CheckForUpgradesScriptFile(this, {
+      providerVersion: props.provider.version,
+      fqproviderName: providerName,
+    });
+
+    new ProviderUpgrade(this, {
+      scriptPath: upgradeScript.path,
+    });
+
+    new JsonFile(this, this.cdktfJson, {
+      obj: {
+        language: 'typescript',
+        app: 'echo noop',
+        sendCrashReports: false,
+        terraformProviders: [`${props.provider.owner}/${props.provider.name}@~> ${props.provider.version}`],
+      },
+    });
+    this.addTask('fetch', {
+      exec: `mkdir -p src/base && rm -rf ./src/base/* && cdktf get && cp -R .gen/providers/${props.provider.name}/* ./src/base/ && cp .gen/versions.json ./src/base/version.json`,
+      env: {
+        CHECKPOINT_DISABLE: '1',
+      },
+    });
+
+    this.setScript(
+      'commit',
+      'git add -A && git commit -am "Update provider" || echo "No changes to commit"',
+    );
+    // eslint-disable-next-line quotes
+    this.setScript(
+      'should-release',
+      '! git diff --exit-code v$(cat version.json | jq -r \'.version\') ./src ./package.json',
+    );
+    this.setScript(
+      'prebump',
+      'yarn fetch && yarn compile && yarn run commit && yarn run should-release',
+    );
+    this.setScript('compile', 'jsii --silence-warnings=reserved-word');
+
+    this.setScript(
+      'build-provider',
+      'yarn fetch && yarn compile && yarn docgen',
+    );
+    this.setScript('test', 'jest --passWithNoTests');
+
+    if (this.npmignore) {
+      this.npmignore.exclude(this.cdktfJson);
+      this.npmignore.exclude('.gen');
+      this.npmignore.exclude('.terraform');
+      this.npmignore.exclude('cdktf.json');
+    }
+    this.gitignore.exclude('.gen');
+    this.gitignore.exclude('.terraform');
+    this.gitignore.exclude('package-lock.json');
+    this.gitignore.exclude(this.cdktfJson);
+  }
+}
---
+++ src/project/provider/provider-upgrade.ts
@@ -0,0 +1,96 @@
+import { JobPermission } from 'projen/lib/github/workflows-model';
+import { NodeProject } from 'projen/lib/javascript';
+
+interface ProviderUpgradeOptions {
+  scriptPath: string;
+}
+
+/**
+ * Checks for new versions of the given provider and creates a PR with an upgrade change if there are changes.
+ */
+export class ProviderUpgrade {
+  constructor(project: NodeProject, options: ProviderUpgradeOptions) {
+    project.addTask('check-if-new-provider-version', {
+      exec: `node ./${options.scriptPath}`,
+    });
+
+    const workflow = project.github?.addWorkflow('provider-upgrade');
+
+    if (!workflow) throw new Error('no workflow defined');
+
+    workflow.on({
+      schedule: [{ cron: '0 3 * * *' }], // Run every day at 3 O'Clock
+      workflowDispatch: {}, // allow manual triggering
+    });
+
+    const newerVersionAvailable =
+      "${{ steps.check_version.outputs.new_version == 'available' }}";
+    const currentVersion = '${{ steps.current_version.outputs.value }}';
+    const newVersion = '${{ steps.new_version.outputs.value }}';
+
+    workflow.addJobs({
+      upgrade: {
+        runsOn: ['ubuntu-latest'],
+        steps: [
+          {
+            name: 'Checkout',
+            uses: 'actions/checkout@v2',
+          },
+          { run: 'yarn install' },
+          {
+            id: 'check_version',
+            run: 'yarn check-if-new-provider-version',
+          },
+          {
+            name: 'get provider current version',
+            if: newerVersionAvailable,
+            id: 'current_version',
+            run: 'echo "value=$(jq -r \'.cdktf.provider.version\' package.json)" >> $GITHUB_OUTPUT',
+          },
+          {
+            run: 'yarn fetch',
+            if: newerVersionAvailable,
+            env: {
+              CHECKPOINT_DISABLE: '1',
+              GH_TOKEN: '${{ secrets.GITHUB_TOKEN }}',
+            },
+          },
+          {
+            name: 'get provider updated version',
+            if: newerVersionAvailable,
+            id: 'new_version',
+            run: 'echo "value=$(jq -r \'. | to_entries[] | .value\' src/version.json)" >> $GITHUB_OUTPUT',
+          },
+          { run: 'yarn compile', if: newerVersionAvailable },
+          { run: 'yarn docgen', if: newerVersionAvailable },
+          {
+            name: 'Set git identity',
+            run: 'git config user.name "github-actions" && git config user.email "github-actions@github.com"',
+          },
+          {
+            name: 'Create Pull Request',
+            if: newerVersionAvailable,
+            uses: 'peter-evans/create-pull-request@v4',
+            with: {
+              'commit-message': `chore: upgrade provider from \`${currentVersion}\` to version \`${newVersion}\``,
+              'branch': 'github-actions/upgrade-main',
+              'title': `chore: upgrade provider from \`${currentVersion}\` to version \`${newVersion}\``,
+              'body': `This PR upgrades provider to version ${newVersion}`,
+              'labels': 'auto-approve',
+              'token': '${{ secrets.PROJEN_GITHUB_TOKEN }}',
+              'committer': 'github-actions <github-actions@github.com>',
+              'author': 'github-actions <github-actions@github.com>',
+              'signoff': true,
+            },
+          },
+        ],
+        permissions: {
+          pullRequests: JobPermission.WRITE,
+          issues: JobPermission.WRITE,
+          contents: JobPermission.WRITE,
+          statuses: JobPermission.WRITE,
+        },
+      },
+    });
+  }
+}
